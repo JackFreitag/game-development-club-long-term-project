6b6108445af0dd1d2e008af8084032e793bcf11a
TuxTheAstronaut <55007230+TuxTheAstronaut@users.noreply.github.com>
2026-05-21 03:56:04 -0400

    basic pause menu toggle

--- scripts/ui/game_menu.gd
+++ scripts/ui/game_menu.gd
@@ -1,1 +1,11 @@
-extends Control
+extends CanvasLayer
+
+func _ready() -> void:
+	Main.game_state_changed.connect(state_changed)
+
+func state_changed(state : Main.game_states) -> void:
+	match state:
+		Main.game_states.Paused:
+			self.show()
+		Main.game_states.Active:
+			self.hide()
